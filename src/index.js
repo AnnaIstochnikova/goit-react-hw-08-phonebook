39d70a27b7357fd272cd1dd10984ce944776b4c8
AnnaIstochnikova <anya.istochnikova@gmail.com>
2023-12-02 17:41:18 +0100

    Register

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 
-import { store } from 'redux/store';
+import store, { storePersist } from 'redux/store';
 import { App } from 'components/App';
 
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate persistor={storePersist} loading={null}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
